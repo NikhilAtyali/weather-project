f9727c25f9fa7ef0189e9c26341addd1e1432108
NikhilAtyali <88334264+NikhilAtyali@users.noreply.github.com>
2022-01-12 13:38:57 +0530

    "Latest"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+
+// Key = 0b138bc936843a1550150afddce0b012
+// API= "https://api.openweathermap.org/data/2.5/"    weather?q={city name}&appid={API key}
+
+const api = {
+  key: "b138bc936843a1550150afddce0b012",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
   return <div className="App">Updated</div>;
